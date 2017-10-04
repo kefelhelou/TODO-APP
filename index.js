579efdfbb743ae69ef51089fd0b93c55dee10b40
kefelhelou <kelhelouwork@gmail.com>
2017-10-04 08:59:27 +0300

    add changes

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 // TO DO APP
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
-// CREAT A HELLO WORLD IN EXPRESS
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
+let arr = ['a', 'b', 'c', 'd'];
 
-// ADD MIDDLEWARE LOG EVERY REQUEST METHOD AND URL
+// ADD MIDDLEWARE LOG EVERY REQUEST METHOD AND URL middleware always has next, the must has next()
+// IT SHOULD BE IN THE TOP
 app.use(function (req, res, next) {
   console.log('Time', Date.now());
   console.log('The request', req.method, req.url);
   next();
 });
 
+app.get('/items', function (req, res) {
+  res.send(arr);
+});
 // SETUP STATIC HANDLER
+app.use('/', express.static('public'));
 
 // LISTEN TO THE PORT
 app.listen(PORT, function () {
   console.log('listnig to the port  :', PORT);
 });
+
+// // CREAT A HELLO WORLD IN EXPRESS
+// app.get('/', function (req, res) {
+//   res.send('hello world');
+// });
