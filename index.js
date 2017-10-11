4c2ca496efaaef3eb9c0f050ae08ad9d7cbd7485
kefelhelou <kelhelouwork@gmail.com>
2017-10-11 09:00:32 +0300

    make my code not hardcode

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // TO DO APP
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 5050;
 
-const items = [{describtion: 'study'}, {describtion: 'play'} ];
+const items = [{describtion: 'study'}, {describtion: 'play'}]; // Defined here to show how can i use xhr to get information from backend
 
 // ADD MIDDLEWARE LOG EVERY REQUEST METHOD AND URL middleware always has next, the must has next()
 // IT SHOULD BE IN THE TOP
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
 });
 
 // SETUP STATIC HANDLER
-app.use('/', express.static('public'));
+app.use('/', express.static('public')); // To read the files of frontend
 
 app.get('/items', function (req, res) {
   res.json(items);
@@ -30,6 +30,8 @@ app.get('/items', function (req, res) {
 app.listen(PORT, function () {
   console.log('listnig to the port  :', PORT);
 });
+
+// app.listen('The app is working on port' + PORT);
 
 // // CREAT A HELLO WORLD IN EXPRESS
 // app.get('/', function (req, res) {
--- public/main.js
+++ public/main.js
@@ -6,44 +6,51 @@
 // if we are using data and sending it in request we have first fo stringfy it "convert from object to string"
 // appendChild will add the item to the end of the element in document
 
+var addTodo = document.getElementById('add');
+var addItem = document.getElementById('additem');
+
 // this variable will define the list from html
 var listContainer = document.querySelector('#list-container'); // querySelector to use the id and class as in css
-// The function that will make the request to the server from the client side
-function request () {
+// // The function that will make the request to the server from the client side
+// function request () { //this is a hardcoded way which means it is not flixible and it's weak way of coding
+//   var xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function () { // <= this is a callback -- we define a callback and saying this is what we want to do
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       var result = JSON.parse(xhr.responseText); // to convert data into defined syntax
+//       renderList(result);
+//     }
+//   };
+//   xhr.open('GET', '/items', true); // to get the items from the url
+//   xhr.send();
+// }
+
+function request (method, url, cb) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () { // <= this is a callback -- we define a callback and saying this is what we want to do
     if (xhr.readyState == 4 && xhr.status == 200) {
       var result = JSON.parse(xhr.responseText); // to convert data into defined syntax
-      renderList(result);
+      cb(result);
     }
   };
-  xhr.open('GET', '/items', true); // to get the items from the url
+  xhr.open(method, url, true); // to get the items from the url
   xhr.send();
 }
 
-request();
+request('GET', '/items', renderList);
 
-// The function that will generate the list
+  // The function that will generate the list
 function renderList (todos/* In side the function i will use this name */) { // converting result and result to html + display to user means render
-  var ul = document.createElement('ul'); // create html element but not displayed in the page
-  // var li = document.createElement('li'); we used it before using forEach
-  // console.log(ul);
-  // console.log(li);
-  // console.log(todos);
-// I need to repeat this part
-  // var desc = todos[0].describtion; // define the first item of our object
-  // console.log(desc);
-  // li.innerText = todos[0].describtion; // put the item into the list
-  // ul.appendChild(li);
-  // console.log(ul);
-  // listContainer.appendChild(ul);
-  todos.forEach(function (todo) {
-    var li = document.createElement('li');
-    li.innerText = todo.describtion;
-    ul.appendChild(li);
+  addTodo.addEventListener('click', function (event) {
+    event.preventDefault();
+    var ul = document.createElement('ul'); // create html element but not displayed in the page
+    todos.forEach(function (todo) {
+      var li = document.createElement('li');
+      li.innerText = todo.describtion;
+      ul.appendChild(li);
+    });
+    listContainer.appendChild(ul);
+    // console.log(todoArray); // to print the result in terminal
   });
-  listContainer.appendChild(ul);
-  // console.log(todoArray); // to print the result in terminal
 }
 
 // Practice on forEach , map and filter
